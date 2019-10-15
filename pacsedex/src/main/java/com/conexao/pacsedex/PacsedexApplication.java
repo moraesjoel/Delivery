0b879518c403963101e84708a1c8e90ba8d09cd4
IsJordanBraz <52468092+IsJordanBraz@users.noreply.github.com>
2019-10-15 16:16:45 -0300

    Conexao com DB

--- pacsedex/src/main/java/com/conexao/pacsedex/PacsedexApplication.java
+++ pacsedex/src/main/java/com/conexao/pacsedex/PacsedexApplication.java
@@ -2,6 +2,9 @@ package com.conexao.pacsedex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,14 +14,23 @@ import java.sql.SQLException;
 public class PacsedexApplication {
 
     public static void main(String[] args) throws SQLException {
+        SpringApplication.run(PacsedexApplication.class, args);
         try {
             Connection conexao = DriverManager.getConnection(
                     "jdbc:postgresql://localhost/postgres",
                     "postgres", "senha");
-            System.out.println("Conectado!");
+            System.out.println("Conectado ao data base!");
             conexao.close();
         } catch(Exception e){
             System.out.println(e);
         }
     }
 }
+@RestController
+class GreetingController{
+    @RequestMapping(value = "/conexao", method = RequestMethod.GET)
+    public String greet(){
+        return "{ preco : 20,00}";
+    }
+}
+
